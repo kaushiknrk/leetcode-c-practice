aa6aa5aefdb1b1652f79f52304c8310fd5e3225d
KAUSHIK NR <kaushiknr326@gmail.com>
2026-07-18 22:23:55 +0530

    4 days continuos streak

---
+++ Daily-Challenge/Find Greatest Common divisor of Array .c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int findGCD(int* nums, int numsSize)
+{
+    int min = nums[0];
+    int max = nums[0];
+
+    for (int i = 1; i < numsSize; i++)
+    {
+        if (nums[i] < min)
+            min = nums[i];
+
+        if (nums[i] > max)
+            max = nums[i];
+    }
+
+    return gcd(min, max);
+}
