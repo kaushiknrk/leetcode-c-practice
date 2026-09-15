524aeda55995cccfb4d36e2d194d6546f5173f64
KAUSHIK NR <kaushiknr326@gmail.com>
2026-09-15 16:41:54 +0530

    Rectangle Overlap 836

---
+++ Geomentry/Rectangle Overlap.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+bool isRectangleOverlap(int* rec1, int rec1Size, int* rec2, int rec2Size) {
+    return max(rec1[0], rec2[0]) < min(rec1[2], rec2[2]) && max(rec1[1], rec2[1]) < min(rec1[3], rec2[3]);
+}
